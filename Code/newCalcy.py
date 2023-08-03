6616e4f896e1e13b377e2195a325bdaf5fc7b717
Nitesh-Suresh <niteshanitha@gmail.com>
2023-08-03 12:28:10 +0530

    Added styles
    Error in styles

--- Code/newCalcy.py
+++ Code/newCalcy.py
@@ -46,190 +46,173 @@ def display_history():
 
 root = tk.Tk()
 root.title("Simple Calculator")
-root.geometry("300x275")
-
+root.geometry("400x380")
+root.configure(bg="#f0f0f0")
 
 #text field
 text_result=tk.Text(
     root,
     height =2,
     width=16,
-    font=("Arial", 24)
+    font=("Arial", 24),
+    bd=5,
+    relief="ridge"
 )
 
-text_result.grid(columnspan=5)
+text_result.grid(columnspan=5, pady=10)
 
-btn_1 = tk.Button(
-    root,
-    text = "1",
-    command= lambda:add_to_calculation(1),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_1.grid(row = 2, column=1)
+def set_button_style(button):
+    button.config(
+        bd=3,  # Add a border
+        relief="raised",  # Set the border style
+        bg="#e0e0e0",  # Set the background color for the buttons
+        activebackground="#d0d0d0",  # Set the background color when the button is pressed
+        font=("Arial", 14)
+    )
 
+buttons =[
+    tk.Button(
+        root,
+        text = "1",
+        command= lambda:add_to_calculation(1),
+        width= 5,
+    ),
 
-btn_2 = tk.Button(
-    root,
-    text = "2",
-    command= lambda:add_to_calculation(2),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_2.grid(row = 2, column=2)
+    tk.Button(
+        root,
+        text = "2",
+        command= lambda:add_to_calculation(2),
+        width= 5,
+    ),
 
-btn_3 = tk.Button(
-    root,
-    text = "3",
-    command= lambda:add_to_calculation(3),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_3.grid(row = 2, column=3)
+    tk.Button(
+        root,
+        text = "+",
+        command= lambda:add_to_calculation("+"),
+        width= 5,
+    ), 
 
-btn_plus = tk.Button(
-    root,
-    text = "+",
-    command= lambda:add_to_calculation("+"),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_plus.grid(row = 2, column=4)
+    tk.Button(
+        root,
+        text = "4",
+        command= lambda:add_to_calculation(4),
+        width= 5,
+    ),
 
+    tk.Button(
+        root,
+        text = "5",
+        command= lambda:add_to_calculation(5),
+        width= 5,
+    ),
 
-btn_4 = tk.Button(
-    root,
-    text = "4",
-    command= lambda:add_to_calculation(4),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_4.grid(row = 3, column=1)
+    tk.Button(
+        root,
+        text = "6",
+        command= lambda:add_to_calculation(6),
+        width= 5,
+    ),
 
-btn_5 = tk.Button(
-    root,
-    text = "5",
-    command= lambda:add_to_calculation(5),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_5.grid(row = 3, column=2)
+    tk.Button(
+        root,
+        text = "-",
+        command= lambda:add_to_calculation("-"),
+        width= 5,
+    ),
 
-btn_6 = tk.Button(
-    root,
-    text = "6",
-    command= lambda:add_to_calculation(6),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_6.grid(row = 3, column=3)
+    tk.Button(
+        root,
+        text = "7",
+        command= lambda:add_to_calculation(7),
+        width= 5,
+    ),
 
-btn_minus = tk.Button(
-    root,
-    text = "-",
-    command= lambda:add_to_calculation("-"),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_minus.grid(row = 3, column=4)
+    tk.Button(
+        root,
+        text = "8",
+        command= lambda:add_to_calculation(8),
+        width= 5,
+    ),
 
-btn_7 = tk.Button(
-    root,
-    text = "7",
-    command= lambda:add_to_calculation(7),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_7.grid(row = 4, column=1)
+    tk.Button(
+        root,
+        text = "9",
+        command= lambda:add_to_calculation(9),
+        width= 5,
+    ),
 
-btn_8 = tk.Button(
-    root,
-    text = "8",
-    command= lambda:add_to_calculation(8),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_8.grid(row = 4, column=2)
+    tk.Button(
+        root,
+        text = "*",
+        command= lambda:add_to_calculation("*"),
+        width= 5,
+    ),
 
-btn_9 = tk.Button(
-    root,
-    text = "9",
-    command= lambda:add_to_calculation(9),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_9.grid(row = 4, column=3)
+    tk.Button(
+        root,
+        text = "(",
+        command= lambda:add_to_calculation("("),
+        width= 5,
+    ),
 
-btn_mul = tk.Button(
-    root,
-    text = "*",
-    command= lambda:add_to_calculation("*"),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_mul.grid(row = 4, column=4)
+    tk.Button(
+        root,
+        text = "0",
+        command= lambda:add_to_calculation(0),
+        width= 5,
+    ),
 
-btn_open = tk.Button(
-    root,
-    text = "(",
-    command= lambda:add_to_calculation("("),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_open.grid(row = 5, column=1)
+    tk.Button(
+        root,
+        text = ")",
+        command= lambda:add_to_calculation(")"),
+        width= 5,
+    ),
 
-btn_0 = tk.Button(
-    root,
-    text = "0",
-    command= lambda:add_to_calculation(0),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_0.grid(row = 5, column=2)
+    tk.Button(
+        root,
+        text = "/",
+        command= lambda:add_to_calculation("/"),
+        width= 5,
+    ),
 
-btn_close = tk.Button(
-    root,
-    text = ")",
-    command= lambda:add_to_calculation(")"),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_close.grid(row = 5, column=3)
+    tk.Button(
+        root,
+        text = "C",
+        command= clear_field,
+        width= 11,
+    ),
 
-btn_div = tk.Button(
-    root,
-    text = "/",
-    command= lambda:add_to_calculation("/"),
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_div.grid(row = 5, column=4)
+    tk.Button(
+        root,
+        text = "M",
+        command= display_history,
+        width= 5,
+    ),
 
-btn_clear = tk.Button(
-    root,
-    text = "C",
-    command= clear_field,
-    width= 11,
-    font= ("Arial", 14)
-)
-btn_clear.grid(row = 6, column=1,columnspan=2)
+    tk.Button(
+        root,
+        text = "=",
+        command= evaluate_calculation,
+        width= 5,
+    )
+] 
 
-btn_memory = tk.Button(
-    root,
-    text = "M",
-    command= display_history,
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_memory.grid(row = 6, column=3)
+# Grid positions for buttons
+button_positions = [
+    (2, 1), (2, 2), (2, 3), (2, 4),
+    (3, 1), (3, 2), (3, 3), (3, 4),
+    (4, 1), (4, 2), (4, 3), (4, 4),
+    (5, 1), (5, 2), (5, 3), (5, 4),
+    (6, 1), (6, 3), (6, 4),
+]
 
-btn_equal = tk.Button(
-    root,
-    text = "=",
-    command= evaluate_calculation,
-    width= 5,
-    font= ("Arial", 14)
-)
-btn_equal.grid(row = 6, column=4)#columspan = 2, width = 11
+for i, button in enumerate(buttons):
+    set_button_style(button)
+    button.grid(
+        row=button_positions[i][0], 
+        column=button_positions[i][1], 
+        pady=5, 
+        padx=5)
 
 root.mainloop()
